258086f689954cdd24615111c46394191e77ed26
Godoy <renanfgfg@gmail.com>
2021-05-04 14:12:28 -0300

    Adicionando controller e método listar

---
+++ src/main/java/com/gxdxy/projeto/controller/ControllerFuncionario.java
@@ -0,0 +1,24 @@
+package com.gxdxy.projeto.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gxdxy.projeto.model.Funcionario;
+import com.gxdxy.projeto.repository.RepositorioFuncionario;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class ControllerFuncionario {
+
+	@Autowired
+	private RepositorioFuncionario repositorioFuncionario;
+
+	@GetMapping("/fucionarios")
+	public List<Funcionario> listarTodosFuncionarios(){
+		return repositorioFuncionario.findAll();
+	}
+}
